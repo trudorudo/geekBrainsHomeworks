07158202329a1367ce203da771cbf69d934d4283
trudorudo <k.trudorudo@gmail.com>
2020-09-29 21:15:07 -0400

    second part

--- src/geekbrains/homeworks/homework1/Main.java
+++ src/geekbrains/homeworks/homework1/Main.java
@@ -10,10 +10,18 @@ public class Main {
         int[] myNums2 = new int[8];
         myNums2 = new int[]{0, 3, 6, 9, 12, 15, 18, 21};
         int[] myNums3 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int[] myNums4 = new int[]{2, 3, 4, 1, 4};
         replaceOneAndZero(myNums);
         createArray(myNums2);
         incrementSmallNumbers(myNums3);
         drawDiagonal();
+         findMinAndMax(myNums2);
+        System.out.println(findMinAndMax(myNums3));
+        findPartsSum(myNums2);
+        // findPartsSum returns true
+        System.out.println(findPartsSum(myNums2));
+        // findPartsSum returns false
+        System.out.println(findPartsSum(myNums4));
     }
 
     private static void replaceOneAndZero(int[] myNums){
@@ -55,4 +63,36 @@ public class Main {
         System.out.println(Arrays.deepToString((myMultiDimensional)));
     }
 
+    private static String findMinAndMax(int[] myNums2) {
+        int maxValue = myNums2[0];
+        int minValue = myNums2[0];
+        for (int i = 0; i < myNums2.length; i++) {
+            if (myNums2[i] > maxValue) {
+                maxValue = myNums2[i];
+            }
+            if(myNums2[i] < minValue){
+                minValue = myNums2[i];
+            }
+        }
+        String result = "Min value is " + minValue + ". Max value is " + maxValue + ".";
+        return result;
+    }
+    private static boolean findPartsSum(int[] myNums2){
+        int sumLeft = 0;
+        int sumRight = 0;
+        for(int i = 0; i < myNums2.length; i++){
+            if(sumLeft <= sumRight){
+                sumLeft+=myNums2[i];
+                i++;
+            } else {
+                int j=myNums2.length-1;
+                sumRight+=myNums2[j];
+                j--;
+            }
+            if(sumLeft != 0 && sumLeft == sumRight ){
+                return true;
+            }
+        }
+        return false;
+    }
 }
