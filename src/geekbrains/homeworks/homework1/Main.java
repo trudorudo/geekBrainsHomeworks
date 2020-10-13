7e43d4e7f86302a5ca8b3d54cf5d4cd1a378c0b5
trudorudo <k.trudorudo@gmail.com>
2020-10-13 18:26:38 -0400

    homework 6

--- src/geekbrains/homeworks/homework1/Main.java
+++ src/geekbrains/homeworks/homework1/Main.java
@@ -6,33 +6,15 @@ import com.sun.codemodel.internal.JForEach;
 public class Main {
 
     public static void main(String[] args){
-        Employee employee1 = new Employee("James Petrov",
-                                        "Web Developer",
-                                          "james@gmail.com",
-                                    "90832342", 90000, 30);
-        employee1.getEmployeeInfo();
+        Cat cat = new Cat(20, 5);
+        Dog dog = new Dog(40,0, 10);
 
+        cat.run();
+        cat.jump();
 
-        Employee[] employeesArray = new Employee[5];
-        employeesArray[0] = new Employee("Ivanov Ivan",
-                                        "Engineer",
-                                        "ivivan@mailbox.com", "892312312", 30000, 30);
-        employeesArray[1] = new Employee("Sergey Sergeev",
-                                       "Manager",
-                                         "serg@mailbox.com", "892312312", 70000, 50);
-        employeesArray[2] = new Employee("Mike Kovalsky",
-                                        "Front End Developer",
-                                          "kovalsky@mailbox.com", "892312312", 30000, 20);
-        employeesArray[3] = new Employee("Ivanova Maria",
-                                        "Lead Engineer",
-                                          "ivivan@mailbox.com", "892312312", 30000, 60);
-        employeesArray[4] = new Employee("Ivanova Masha",
-                                        "Engineer",
-                                           "ivivamasha@mailbox.com", "892312312", 20000, 31);
-        for (Employee employee : employeesArray) {
-            employee.printOlderThen40();
-        }
+        dog.run();
+        dog.swim();
+        dog.jump();
     }
-
 }
 
