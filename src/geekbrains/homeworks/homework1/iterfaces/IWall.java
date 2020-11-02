e8a2af2b73f9dc9ed9675b4d9c9f28e8ef053db2
trudorudo <k.trudorudo@gmail.com>
2020-11-01 21:07:28 -0500

    java2, homework2

--- src/geekbrains/homeworks/homework1/iterfaces/IWall.java
+++
@@ -1,5 +0,0 @@
-package geekbrains.homeworks.homework1.iterfaces;
-
-public interface IWall {
-    boolean ifCanJump(int jumpingHeight);
-}
---
+++ src/geekbrains/homeworks/java2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package geekbrains.homeworks.java2;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException(String msg){
+        super(msg);
+    }
+}
