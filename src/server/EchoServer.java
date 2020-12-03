fa60e1c4904d02ad242c9e3b16cf7e5b4a9762fe
trudorudo <k.trudorudo@gmail.com>
2020-12-03 13:26:00 -0500

    hw

--- src/server/EchoServer.java
+++ src/server/EchoServer.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoServer {
     public static final int SERVER_PORT = 8080;
@@ -18,18 +19,43 @@ public class EchoServer {
             DataInputStream in= new DataInputStream(clientSocket.getInputStream());
             DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
-            while(true){
-                String message = in.readUTF();
-                System.out.println("Message: " + message);
-                if(message.equals("/exit")){
-                    break;
-                }
-                out.writeUTF("Server response" + message.toUpperCase());
-            }
+            Thread inputThread = null;
+            inputThread = runInputThread(in);
+            runOutputThread(out);
             System.out.println("Server has stopped running");
         } catch (IOException e) {
             System.out.println("Port is already in use");
             e.printStackTrace();
         }
+    }
+
+    private static void runOutputThread(DataOutputStream out) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String message = scanner.next();
+            out.writeUTF(message);
+            if(message.equals("/end")){
+                break;
+            }
+        }
+    }
+
+    private static Thread runInputThread(DataInputStream in) {
+        Thread thread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String message = in.readUTF();
+                    System.out.println("Message: " + message);
+                    if (message.equals("/exit")) {
+                        break;
+                    }
+                } catch (IOException e) {
+                    System.out.println("Connection was closed");
+                    break;
+                }
+            }
+            });
+        thread.start();
+        return thread;
     }
 }
